a479de431f70480e3e309d242d1f481660d12a44
Nino678190 <superwauzi@gmx.de>
2025-09-06 15:50:34 +0200

    Entferne nicht benötigte Importe aus der app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, render_template
-import json
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-import os
 
 app = Flask(__name__)
 

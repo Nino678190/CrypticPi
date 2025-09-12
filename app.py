bfdbed27af3ad534eab69df13f2e1d6e9adc353d
Nino678190 <superwauzi@gmx.de>
2025-09-12 20:01:10 +0200

    Behebe die Verarbeitung von verschlüsselten Nachrichten in der sendMessage-Funktion und optimiere die Dekodierung im decrypt-Prozess.

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def sendMessage():
             except UnicodeDecodeError:
                 return render_template('ergebnis.html', message="Fehler: Die Datei muss UTF-8 kodiert sein.")
     print(request.form.get('encrypt'))
-    if request.form.get('encrypt') == 'encrypt': #TODO Fix function
+    if request.form.get('encrypt') == 'encrypt':
         return send_message(message, password, file)
     else:
         if fileCheck == True:
@@ -67,7 +67,7 @@ def send_message(message, password, file=False):
     cipher = Cipher(algorithms.AES(encryption_key), modes.ECB(), backend=default_backend()) # Auch möglich mit modes.GMC()
     encryptor = cipher.encryptor()
     ciphertext = encryptor.update(message) + encryptor.finalize()
-    ciphertext = ciphertext.hex() # Convert to string for easy transmission
+    ciphertext = ciphertext.hex()
     if len(ciphertext) > 250:
         # Create in-memory file
         mem_file = BytesIO()
@@ -82,27 +82,26 @@ def send_message(message, password, file=False):
         )
     return render_template('ergebnis.html', message=ciphertext)
 
-
-@app.route('/decrypt', methods=['POST'])
 def decrypt(ciphertext, password, file=False):
     try:
         ciphertext = ciphertext.strip()
-        ciphertext = ciphertext.encode()
+        ciphertext = bytes.fromhex(ciphertext.decode())  # Convert back to bytes
 
         password = password.strip()
         password = password.encode()
         if not ciphertext or not password:
             return render_template('ergebnis.html', message="Fehler: Nachricht und Passwort dürfen nicht leer sein.")
-        ciphertext = bytes.fromhex(ciphertext.decode())  # Convert back to bytes
+        
         encryption_key = encryption_key_gen(password)
         decipher = Cipher(algorithms.AES(encryption_key), modes.ECB(), backend=default_backend()) # Auch möglich mit modes.GMC()
         decryptor = decipher.decryptor()
         decrypted_text = decryptor.update(ciphertext) + decryptor.finalize()
         print(decrypted_text)
-        # decrypted_text = base64.b64decode(decrypted_text)  # Decode from base64
-        # print(decrypted_text)
+
         if not file:
             decrypted_text = decrypted_text.decode('utf-8').strip()
+        if decrypted_text.__contains__(b'\x0b'):
+            decrypted_text.replace(b'\x0b', b'')
         if file:            
             # Create in-memory file
             mem_file = BytesIO()

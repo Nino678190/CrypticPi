9598719cccc73fb0fe063ed68cde825f7d9354b9
Nino678190 <superwauzi@gmx.de>
2025-09-27 11:33:17 +0200

    Entferne die Dateiupload-Option aus dem Formular und passe die sendMessage-Funktion an, um die Verarbeitung von Nachrichten zu vereinfachen.

--- app.py
+++ app.py
@@ -27,33 +27,18 @@ def encryption_key_gen(password):
 
 @app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 @app.route('/sendMessage', methods=['POST'])
 def sendMessage():
     message = request.form.get('message')
     password = request.form.get('password')
-    fileCheck = request.form.get('fileCheck')
-    file = False
-    fileExtension = None
-    if 'file' in request.files:
-        file = request.files['file']
-        if file and file.filename:
-            try:
-                fileExtension = os.path.splitext(file.filename)[1]
-                message = file.read()
-                file = True
-            except UnicodeDecodeError:
-                return render_template('ergebnis.html', message="Fehler: Die Datei muss UTF-8 kodiert sein.")
-    print(request.form.get('encrypt'))
-    if request.form.get('encrypt') == 'encrypt': #TODO Fix function
-        return send_message(message, password, file, fileExtension)
+    if request.form.get('encrypt') == 'encrypt':
+        return send_message(message, password)
     else:
-        if fileCheck == True:
-            file = True
-        return decrypt(message, password, file)
+        return decrypt(message, password)
 
-def encrypt(message, password, file=False):
+def encrypt(message, password):
     try: 
         padding_length = 16 - (len(message) % 16)
         message = message + bytes([padding_length] * padding_length)
@@ -66,17 +51,14 @@ def encrypt(message, password, file=False):
     except Exception as e:
         return render_template('ergebnis.html', message="Fehler: " + str(e))
 
-def send_message(message, password, file=False, fileExtension=None):
+def send_message(message, password):
     if not isinstance(message, bytes):
         message = message.encode()
     password = password.encode()
     if not message or not password:
         return render_template('ergebnis.html', message="Fehler: Nachricht und Passwort dürfen nicht leer sein.")
     # Pad the message to be a multiple of 16 bytes
-    ciphertext = encrypt(message, password, file)
-    fileExt = encrypt(fileExtension.encode(), password) if fileExtension else ''
-    if file:
-        ciphertext = fileExt + '|' + ciphertext
+    ciphertext = encrypt(message, password)
     if len(ciphertext) > 250:
         # Create in-memory file
         mem_file = BytesIO()
@@ -101,52 +83,22 @@ def decrypt_message(ciphertext, password):
     except Exception as e:
         return render_template('ergebnis.html', message="Fehler: " + str(e))
 
-def decrypt(ciphertext, password, file=False):
+def decrypt(ciphertext, password):
     try:
-        fileExtension = ciphertext.split(b'|', 1)[0] if file else None
-        print(fileExtension)
+
         ciphertext = ciphertext.strip()
-        if (file):
-            ciphertext = ciphertext.split(b'|', 1)[1]
-            ciphertext = bytes.fromhex(ciphertext.decode())  # Convert back to bytes
-        else :
-            ciphertext = bytes.fromhex(ciphertext)  # Convert back to bytes
+
+        ciphertext = bytes.fromhex(ciphertext)  # Convert back to bytes
         password = password.strip()
         password = password.encode()
         if not ciphertext or not password:
             return render_template('ergebnis.html', message="Fehler: Nachricht und Passwort dürfen nicht leer sein.")
         # ciphertext = bytes.fromhex(ciphertext.encode())  # Convert back to bytes
         decrypted_text = decrypt_message(ciphertext, password)
-        
 
-        if file:
-            fileExt = decrypt_message(bytes.fromhex(fileExtension.decode()), password)
-            fileExt = fileExt.decode()
-            if '.' in fileExt:
-                fileExtension = fileExt
-            else:
-                fileExtension = '.txt'
-        # decrypted_text = base64.b64decode(decrypted_text)  # Decode from base64
-        # print(decrypted_text)
-        if not file:
-            decrypted_text = decrypted_text.decode('utf-8').strip()
-            if '\x0c' in decrypted_text:  # Use string version for decoded text
-                decrypted_text = decrypted_text.replace('\x0c', '')
-        else:
-            if b'\x0b' in decrypted_text:  # Use bytes version for file data
-                decrypted_text = decrypted_text.replace(b'\x0b', b'')
-        if file:            
-            # Create in-memory file
-            mem_file = BytesIO()
-            mem_file.write(decrypted_text)
-            mem_file.seek(0)
-
-            return send_file(
-                mem_file,
-                as_attachment=True,
-                download_name='decrypted_message' + fileExtension,
-                mimetype='text/' + fileExtension.lstrip('.')
-            )
+        decrypted_text = decrypted_text.decode('utf-8').strip()
+        if '\x0c' in decrypted_text:  # Use string version for decoded text
+            decrypted_text = decrypted_text.replace('\x0c', '')
         return render_template('ergebnis.html', message=decrypted_text)
     except UnicodeDecodeError:
         return render_template('ergebnis.html', message="Fehler: Falscher Input oder Passwort")
